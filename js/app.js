e64ebcbcaafa62b4f6863988c73c04ed4a228b53
sowmyakallu <skallu@computerim.com>
2018-03-14 20:43:46 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,71 @@
+
+var app=angular.module("HangmanApp",[]);
+app.controller("GameController",['$scope', function($scope) {
+var words =["Cat","Rat","Mat","Bat"];	
+	$scope.incorrectLettersChosen =[];
+    $scope.correctLettersChosen =[];
+	$scope.guesses = 6;
+	$scope.displayWord = '';
+	$scope.input = {
+		letter : ''
+	}
+var selectRandomWord = function(){
+	var index = Math.round(Math.random()*words.length);
+	return words[index];
+}
+var newGame = function(){
+	$scope.incorrectLettersChosen =[];
+    $scope.correctLettersChosen =[];
+	$scope.guesses = 6;
+	$scope.displayWord = '';
+	
+	selectedWord =selectRandomWord();
+	var tempDisplayWord = '';
+	console.log(selectedWord);
+	for (var i = 0;i < selectedWord.length;i++) {
+		tempDisplayWord += '*';
+	    }
+		$scope.displayWord = tempDisplayWord;
+}
+$scope.letterChosen = function(){
+	
+	for(var i = 0;i < $scope.correctLettersChosen.length;i++){
+		if($scope.correctLettersChosen[i].toLowerCase()==$scope.input.letter.toLowerCase()){
+			$scope.input.letter = "";
+			return;
+		}
+	}
+    for(var i = 0;i < $scope.incorrectLettersChosen.length;i++){
+		if($scope.incorrectLettersChosen[i].toLowerCase()==$scope.input.letter.toLowerCase()){
+			$scope.input.letter = "";
+			return;
+		}
+	}
+	var correct = false;
+	for (var i =0;i < selectedWord.length; i++){
+	    if(selectedWord[i].toLowerCase()==$scope.input.letter.toLowerCase()){
+		   $scope.displayWord = $scope.displayWord.slice(0,i)+$scope.input.letter.toLowerCase()+$scope.displayWord.slice(i+1);
+		   correct = true;
+		}
+	}
+    if(correct){
+       $scope.correctLettersChosen.push($scope.input.letter.toLowerCase());
+	}else{
+	   $scope.guesses--;
+       $scope.incorrectLettersChosen.push($scope.input.letter.toLowerCase());	
+	}
+       $scope.input.letter = "";
+     if($scope.guesses == 0){
+      alert("You lost oh! ");
+     }
+	 if($scope.displayWord.indexOf("*") == -1){
+      alert("You won yeah! ");
+     }
+	 
+	 
+     	 
+}	
+	
+newGame();
+	
+}]);
